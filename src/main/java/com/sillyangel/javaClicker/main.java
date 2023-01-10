7c5246ff6c61c1f8a0a0891a3bb196f70ab490f9
sillyangel <sillyangel3@gmail.com>
2023-01-10 22:34:24 +0000

    stuff

--- src/main/java/com/sillyangel/javaClicker/main.java
+++ src/main/java/com/sillyangel/javaClicker/main.java
@@ -1,1 +1,32 @@
-package com.sillyangel.javaClicker;
+package com.sillyangel.javaclicker;
+
+// awt
+
+import java.awt.BorderLayout;
+import java.awt.GirdLayout;
+
+// swing gui
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI {
+
+    public GUI() {
+        JFrame frame = new JFrame();
+
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GirdLayout(0, 1));
+
+        frame.add(panel, BorderFactory.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Java Clicker - v1");
+        frame.pack();
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        new GUI();
+    }
+}
