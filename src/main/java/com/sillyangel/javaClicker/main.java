570a4069112a2b4d1ccde2fba433028d25289221
sillyangel <sillyangel3@gmail.com>
2023-01-10 23:18:50 +0000

    AAA

--- src/main/java/com/sillyangel/javaClicker/main.java
+++ src/main/java/com/sillyangel/javaClicker/main.java
@@ -3,22 +3,33 @@ package com.sillyangel.javaclicker;
 // awt
 
 import java.awt.BorderLayout;
-import java.awt.GirdLayout;
+import java.awt.GridLayout;
 
 // swing gui
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
-public class GUI {
+public class GUI implements ActionListener{
+
+    int count = 0;
 
     public GUI() {
         JFrame frame = new JFrame();
 
+        JButton button = new JButton("Click me");
+        button.addActionListener(this);
+
+        JLabel label = new JLabel("Number of clicks: 0");
+
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GirdLayout(0, 1));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(label);
+        panel.add(button);
 
         frame.add(panel, BorderFactory.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -26,7 +37,13 @@ public class GUI {
         frame.pack();
         frame.setVisible(true);
     }
+
     public static void main(String[] args) {
         new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
